c16e45afc33dd6060b43d1d8da13421a53883335
Reptartheman <117610334+Reptartheman@users.noreply.github.com>
2023-05-22 21:18:08 -0400

    added some stuff
    Added timer.js and some click sounds. Next is to change the styling, add different color schemes and choices of sounds.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import Timer from './timer.js';
 const tempoDisplay = document.querySelector('.tempo');
 const tempoText = document.querySelector('.tempo-text');
 const decreaseTempoBtn = document.querySelector('.decrease-tempo');
@@ -8,8 +9,14 @@ const subtractBeats = document.querySelector('.subtract-beats');
 const addBeats = document.querySelector('.add-beats');
 const measureCount = document.querySelector('.measure-count');
 
+const click1 = new Audio('./click1.wav');
+const click2 = new Audio('./click2.wav');
+
+
 let bpm = 140;
 let beatsPerMeasure = 4;
+let count = 0;
+let isRunning = false;
 let tempoTextString = 'Mid';
 
 decreaseTempoBtn.addEventListener('click', () => {
@@ -36,6 +43,7 @@ subtractBeats.addEventListener('click', () => {
     }
     beatsPerMeasure--;
     measureCount.textContent = beatsPerMeasure;
+    count = 0;
 });
 
 addBeats.addEventListener('click', () => {
@@ -44,11 +52,26 @@ addBeats.addEventListener('click', () => {
     }
     beatsPerMeasure++;
     measureCount.textContent = beatsPerMeasure;
+    count = 0;
+});
+
+startStopBtn.addEventListener('click', () => {
+    count = 0;
+    if (!isRunning) {
+        metronome.start();
+        isRunning = true;
+        startStopBtn.textContent = 'STOP';
+    } else {
+        metronome.stop();
+        isRunning = false;
+        startStopBtn.textContent = 'START';
+    }
 });
 
 const updateMetronome = () => {
     tempoDisplay.textContent = bpm;
     tempoSlider.value = bpm;
+    metronome.timeInterval = 60000 / bpm;
     switch(true) {
         case (bpm <= 40):
             tempoTextString = 'Grown up tempo';
@@ -91,3 +114,22 @@ const validateTempo = () => {
         return;
     };
 };
+
+function playClick() {
+    if (count === beatsPerMeasure) {
+        count = 0;
+    }
+    if (count === 0) {
+        click1.play();
+        click1.currentTime = 0;
+    } else {
+        click2.play();
+        click2.currentTime = 0;
+    }
+    count++;
+}
+
+
+const metronome = new Timer(playClick, 60000 / bpm, { immediate: true });
+
+
